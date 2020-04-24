8dfc7bb9a9a3ac14d4bcaea71803afb1294c84f2
Cintrol <szk~b9ktPF>
2020-04-25 02:22:20 +0300

    less2 final

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main.views import main_view
+from list_item.views import list_item_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
