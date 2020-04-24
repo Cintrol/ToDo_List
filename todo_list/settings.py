efd1e90a0b78cd097785c835aa5067c4bb7e27de
Cintrol <szk~b9ktPF>
2020-04-24 23:34:18 +0300

    less2

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql',
+        'HOST': "127.0.0.1",
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': '2019',
+        'PORT': 5432
+    },
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
