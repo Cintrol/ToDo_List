9631673e6eac3bf610935b60c48d9e8024b472e2
Cintrol <szk~b9ktPF>
2020-04-26 01:30:27 +0300

    3 урок. Проблема в index.html, не срабатывает импорт {% load get_blocks %} из master.html, приходится повторно импортироватm его же в index

--- list_item/admin.py
+++ list_item/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from list_item.models import ListItemModel
 
-# Register your models here.
+
+class ListItemAdmin(admin.ModelAdmin):
+    """
+    Регистрируем модель по задачам в админке
+    """
+    list_display = ['id','name', 'created','modified','list', 'is_done','expare_date']
+    list_filter = ['name', 'list']
+    search_fields = ['name', 'is_done']
+
+admin.site.register(ListItemModel, ListItemAdmin)
--- list_item/models.py
+++ list_item/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class ListItemModel(models.Model):
+ """
+ Модель списка дел внутри задачи
+ """
+ name = models.CharField(max_length=130, verbose_name='Название дела')
+ created = models.DateTimeField(auto_now_add=True)
+ modified = models.DateTimeField(auto_now=True)
+ list = models.ForeignKey('main.ListModel', on_delete=models.CASCADE)
+ is_done = models.BooleanField(default=False)
+ expare_date = models.DateTimeField(auto_now=True)
+
+
+ def __str__(self):
+  return self.name
+
+ class Meta:
+  verbose_name = 'Список задач'
+
