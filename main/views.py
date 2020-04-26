73a09ca6d3aef12b80923d90b303713f87a046ab
Cintrol <szk~b9ktPF>
2020-04-27 01:59:42 +0300

    3 урок. Проблема в index.html, не срабатывает импорт {% load get_blocks %} из master.html, приходится повторно импортироватm его же в index

--- main/views.py
+++ main/views.py
@@ -1,27 +1,13 @@
 from django.shortcuts import render
-
-
-# Create your views here.
-#("work", True),
-#("home", False),
-#("learn", True)
-
-data = {
-    'lists':[
-        {'id':1, 'name': 'work', 'is_done': False},
-        {'id':2, 'name': 'home', 'is_done': False},
-        {'id':3, 'name': 'learn','is_done' : True}
-    ],
-    'user_name': 'admin'
-}
-
-#for list, in_done in data['lists']:
-#    name = list[0]
-#    is_done = list[1]
+from main.models import ListModel
 
 def main_view(request, pk=0):
-    context = data
+    lists = ListModel.objects.all()
+    lists_one = ListModel.objects.get(name='Learn')
+    lists_filter = ListModel.objects.filter(name='Learn')
+    context = dict(lists=lists, user=request.user)
     return render(request, 'index.html', context)
+
 
 def edit_view(request, pk):
    return 'hello'
